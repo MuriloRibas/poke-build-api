8d6962c388c10b4a0cf9950b0269e0729522f0c4
MuriloRibas <muriloribasss@hotmail.com>
2020-10-16 14:44:32 -0300

    change migrate

--- db/migrate/20201014190300_change_pokemon_column_to_text.rb
+++ db/migrate/20201014190300_change_pokemon_column_to_text.rb
@@ -1,5 +1,5 @@
 class ChangePokemonColumnToText < ActiveRecord::Migration[6.0]
   def change
-    change_column :teams, :pokemons, :text, default: []
+    change_column :teams, :pokemons, :text, default: [].yaml
   end
 end
