3d22ec6462c327d8747450611b455682fda377d0
MuriloRibas <muriloribasss@hotmail.com>
2020-10-16 15:36:56 -0300

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'pages#index'
 
   namespace :api do
     namespace :v1 do
@@ -9,5 +8,4 @@ Rails.application.routes.draw do
     end
   end
 
-  get '*path', to: 'pages#index', via: :all
 end
